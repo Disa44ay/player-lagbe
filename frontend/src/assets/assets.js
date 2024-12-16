43671ee5d0895784e629f6906fc5f14f4d9469d6
Sayeed <abdullahsayeed62@gmail.com>
2024-12-16 11:27:59 +0600

    shop update

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -3,11 +3,58 @@ import logo_old_b_bg from './logo_old_b-bg.png'
 import logo_new_t_bg from './logo_new_t-bg.png'
 import logo_new_w_bg from './logo_new_w-bg.png'
 import logo_new_grayscale from './logo_new_grayscale.png'
+import logo_new_white_t_bg from './logo_new_white_t_bg.png'
+
 import icon_cart from './icons-cart.gif'
 import header_temp from './header_temp.png'
-import logo_new_white_t_bg from './logo_new_white_t_bg.png'
+
+
 import icon_cross from './icon_cross.gif'
 import icon_cross_white from './icon_cross_white.png'
+
+import cone_1 from './cone_1.png'
+import cone_2 from './cone_2.png'
+import cone_3 from './cone_3.png'
+import cone_4 from './cone_4.png'
+import cone_5 from './cone_5.png'
+
+import football_1 from './football_1.png'
+import football_2 from './football_2.png'
+import football_3 from './football_3.png'
+import football_4 from './football_4.png'
+import football_5 from './football_5.png'
+import football_6 from './football_6.png'
+import football_7 from './football_7.png'
+
+import shinguard_1 from './shinguard_1.png'
+import shinguard_2 from './shinguard_2.png'
+import shinguard_3 from './shinguard_3.png'
+import shinguard_4 from './shinguard_4.png'
+import shinguard_5 from './shinguard_5.png'
+import shinguard_6 from './shinguard_6.png'
+
+import jersey_1 from './jersey_1.png'
+import jersey_2 from './jersey_2.png'
+import jersey_3 from './jersey_3.png'
+import jersey_4 from './jersey_4.png'
+import jersey_5 from './jersey_5.png'
+import jersey_6 from './jersey_6.png'
+import jersey_7 from './jersey_7.png'
+import jersey_8 from './jersey_8.png'
+
+import turf_1 from './turf_1.png'
+import turf_2 from './turf_2.png'
+import turf_3 from './turf_3.png'
+import turf_4 from './turf_4.png'
+import turf_5 from './turf_5.png'
+import turf_6 from './turf_6.png'
+import turf_7 from './turf_7.png'
+
+
+
+
+
+
 
 export const assets = {
     logo_old_t_bg,
@@ -19,12 +66,28 @@ export const assets = {
     header_temp,
     logo_new_white_t_bg,
     icon_cross,
-    icon_cross_white
+    icon_cross_white,
 }
 
 export const item_list = [
     {
         item_name: "Football",
-        item_image: " "
+        item_image: football_1
+    },
+    {
+        item_name: "Jersey",
+        item_image: jersey_1
+    },
+    {
+        item_name: "Shinguard",
+        item_image: shinguard_1
+    },
+    {
+        item_name: "Turf boots",
+        item_image: turf_1
+    },
+    {
+        item_name: "Training cone",
+        item_image: cone_1
     }
 ]
--- frontend/src/pages/Shop/Shop.jsx
+++ frontend/src/pages/Shop/Shop.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import './Shop.css'
+import { item_list } from '../../assets/assets';
+
 
 const Shop = () => {
   return (
-    <div>
-      
+    <div className="menu-list" id="menu-list">
+      <h1>Explore our items..</h1>
+      <p className="menu-list-text">
+        We will update our products list based on your response.
+      </p>
+      <div className="menu-list-display">
+        {item_list.map((item,index) => {
+          return (
+          <div key={index} className="menu-list-items">
+            <img src={item.item_image} alt="" />
+            <p>{item.item_name}</p>
+          </div>
+          )
+        })}
+      </div>
+
+      <hr />
     </div>
-  )
-}
+  );
+};
 
 export default Shop
